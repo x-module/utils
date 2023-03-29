caa296ff7271a4b7b1b499d0ae2c85d250626ca3
tudou <tudou@tudou.com>
2023-03-29 11:09:41 +0800

    fix

--- dirver/mysql.go
+++ dirver/mysql.go
@@ -43,9 +43,7 @@ type LinkParams struct {
 // InitializeDB 初始化管理后台数据库
 func InitializeDB(params LinkParams) (*gorm.DB, error) {
 	linkParams := "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-
 	dsn := fmt.Sprintf(linkParams, params.UserName, params.Password, params.Host, params.Port, params.DbName)
-	fmt.Println(dsn)
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
@@ -58,9 +56,6 @@ func InitializeDB(params LinkParams) (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: newLogger,
 	})
-
-	fmt.Println(err)
-	fmt.Println("--------------------------")
 	xerror.PanicErr(err, global.ConnectMysqlErr.String())
 	// 链接池设置
 	// db.DB().SetMaxOpenConns(params.MaxOpenConn)
