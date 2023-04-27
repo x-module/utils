d87d0a84b050bea0db0710eb93e9db76af274fc1
Your Name <244395692@qq.com>
2023-04-28 00:03:05 +0800

    fix

--- dirver/mysql.go
+++ dirver/mysql.go
@@ -58,8 +58,9 @@ func InitializeDB(params LinkParams) (*gorm.DB, error) {
 	})
 	xerror.PanicErr(err, global.ConnectMysqlErr.String())
 	// 链接池设置
-	// db.DB().SetMaxOpenConns(params.MaxOpenConn)
-	// db.DB().SetMaxIdleConns(params.MaxIdleConn)
+	sqlDb, _ := db.DB()
+	sqlDb.SetMaxOpenConns(params.MaxOpenConn)
+	sqlDb.SetMaxIdleConns(params.MaxIdleConn)
 	// db.LogMode(params.Mode == DebugMode)
 	// db.LogMode(false)
 	return db, nil
